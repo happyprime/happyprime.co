5c5d49cfcd46ecddcf54ee1fd4376a710dca8516
jeremyfelt <jeremy.felt@gmail.com>
2025-06-12 14:50:54 -0700

    Update composer dependencies

--- mu-plugins/happyprime/wp/editor.php
+++ mu-plugins/happyprime/wp/editor.php
@@ -16,7 +16,7 @@ add_action( 'plugins_loaded', __NAMESPACE__ . '\remove_default_actions' );
 /**
  * Remove the default duotone SVGs output by WordPress.
  */
-function remove_default_actions() {
+function remove_default_actions(): void {
 	remove_action( 'wp_body_open', 'wp_global_styles_render_svg_filters' );
 	remove_action( 'in_admin_header', 'wp_global_styles_render_svg_filters' );
 }
