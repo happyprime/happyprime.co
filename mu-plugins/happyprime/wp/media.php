b96af745beb73d2edbb83512e270245141702a4e
Viktor Szépe <viktor@szepe.net>
2025-06-13 06:55:26 +0200

    Use built-in constant for megabytes

--- mu-plugins/happyprime/wp/media.php
+++ mu-plugins/happyprime/wp/media.php
@@ -21,7 +21,5 @@ add_filter( 'upload_size_limit', __NAMESPACE__ . '\increase_max_upload_size', 20
  */
 function increase_max_upload_size( $size ): int {
 	// 100 MB (weird # because it is x 1024 bytes).
-	$size = 105000000;
-
-	return $size;
+	return 100 * MB_IN_BYTES;
 }
